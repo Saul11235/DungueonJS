e439532b02c8fdc129f536bdeac45c49df8e2fe8
Edwin Saul <edwinsaulpm@gmail.com>
2021-10-09 00:17:41 -0500

    editado html y js

--- js/MotorJuego.js
+++ js/MotorJuego.js
@@ -1,3 +1,17 @@
- function empiezaJuego() {
-console.log("hola mundo")
+/*declarando variables locales*/
+var canvas;
+var ctx;
+var FPS=40;
+//Dimensiones
+var PantallaAlto=800;
+var PantallaAncho=1000;
+
+  
+
+
+function empiezaJuego() {
+  canvas=document.getElementByID('pantallaJuego')
+  canvas.width=PantallaAncho
+  canvas.heigh=PantallaAlto
+
  }
